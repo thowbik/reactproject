e71702c3abb2c24aaf5e4452d8f9634e3f573294
thowfik-at-840620226347 <hqyklnVn1MRttxCa7W8rAWXDZ+9BHhRj3hZCAwEK3dk=>
2021-05-05 12:15:34 +0530

    components added

---
+++ src/App/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../helpers';
+import { alertActions } from '../actions';
+import { PrivateRoute } from '../components';
+import { HomePage } from '../Pages/HomePage';
+import { LoginPage } from '../Pages/LoginPage';
+import { RegisterPage } from '../Pages/RegisterPage';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        history.listen((location, action) => {
+            this.props.clearAlerts();
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Router history={history}>
+                            <Switch>
+                                <PrivateRoute exact path="/" component={HomePage} />
+                                <Route path="/login" component={LoginPage} />
+                                <Route path="/register" component={RegisterPage} />
+                                <Redirect from="*" to="/" />
+                            </Switch>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { alert } = state;
+    return { alert };
+}
+
+const actionCreators = {
+    clearAlerts: alertActions.clear
+};
+
+const connectedApp = connect(mapState, actionCreators)(App);
+export { connectedApp as App };
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+}
